1099faf55b2ff9644fe78958b8d322c889ac5c30
Arnaldo Menezes <devops.arnaldomenezes@outlook.com>
2022-04-28 00:33:55 -0300

    Organização dos exercícios

--- git-estudos/Atividades/projeto-java-grupo4/src/datas/DatesAndTime.java
+++ git-estudos/Atividades/projeto-java-grupo4/src/datas/DatesAndTime.java
@@ -7,7 +7,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 /**
- * @author diegocardosodev
+ * @author devops-arnaldomenezes
  * @version 1.0
  */
 public class DatesAndTime {
